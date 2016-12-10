4f690fe4c4903f80f1b48aa7a80f3166a0be1181
rjgallac@hotmail.com <rjgallac@hotmail.com>
2016-12-10 22:13:03 +0000

    applicants and jobs added

--- src/main/resources/static/controllers/controllers.js
+++ src/main/resources/static/controllers/controllers.js
@@ -1,8 +1,19 @@
 angular.module('workflowApp')
-    .controller('workflowCtrl', function ($scope, workflowService) {
+    .controller('workflowCtrl', function ($scope, applicationService, jobService, applicantService) {
         applications = [];
-        workflowService.getApplications().then(function(data){
+        jobs = [];
+        applicants = [];
+
+        applicationService.getApplications().then(function(data){
             $scope.applications = data;
+            console.log(data);
+        })
+        jobService.getJobs().then(function(data){
+            $scope.jobs = data;
+            console.log(data);
+        })
+        applicantService.getApplicants().then(function(data){
+            $scope.applicants = data;
             console.log(data);
         })
     })
--- src/main/resources/static/services/services.js
+++ src/main/resources/static/services/services.js
@@ -1,9 +1,35 @@
 
 angular.module('workflowApp')
-    .service('workflowService', function($http, $q) {
+    .service('applicationService', function($http, $q) {
         this.getApplications = function() {
             var defer = $q.defer();
             $http.get('/application/')
+                .success(function(data) {
+                    defer.resolve(data);
+                })
+                .error(function(err, status) {
+                    defer.reject(err);
+                });
+            return defer.promise;
+        }
+    })
+    .service('jobService', function($http, $q) {
+        this.getJobs = function() {
+            var defer = $q.defer();
+            $http.get('/job/')
+                .success(function(data) {
+                    defer.resolve(data);
+                })
+                .error(function(err, status) {
+                    defer.reject(err);
+                });
+            return defer.promise;
+        }
+    })
+    .service('applicantService', function($http, $q) {
+        this.getApplicants = function() {
+            var defer = $q.defer();
+            $http.get('/applicant/')
                 .success(function(data) {
                     defer.resolve(data);
                 })
