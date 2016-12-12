f72ff9ca4c7e18e70c33ebf49585fc0603bf52f8
rjgallac@hotmail.com <rjgallac@hotmail.com>
2016-12-12 22:13:10 +0000

    styling and accordion

--- src/main/java/com/rob/workflow/model/Job.java
+++ src/main/java/com/rob/workflow/model/Job.java
@@ -1,8 +1,11 @@
 package com.rob.workflow.model;
 
+import com.rob.workflow.model.shortworkflow.WorkflowState;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Job {
@@ -12,6 +15,11 @@ public class Job {
     private Long jobId;
 
     private String name;
+
+    @Transient
+    private WorkflowState workflowState;
+
+    private String workflowStateString;
 
     public Job() {
     }
